1310cd122df29968f4e1146ffc3b035de8369de9
lyzsa <susanli0902@outlook.com>
2025-09-09 21:14:34 -0400

    multiple method added

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,16 +2,21 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+
+    }
+
+
+    public static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i < 10; i++) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
 
             if (divisibleBy3 || divisibleBy5) {
                 count++;
             }
 
-
         }
+        return count;
     }
 }
